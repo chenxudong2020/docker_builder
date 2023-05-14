0c1c0598c2ad321178f82a8746f08591a46c323c
chenxudong2020 <872603935@qq.com>
2023-05-14 09:17:02 +0000

    修复启动

--- run.sh
+++ run.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-DERP_HOST=$1
-DERP_CERTS=$2
-DERP_STUN=$3
-DERP_ADDR=$4
-DERP_HTTP_PORT=$5
-DERP_VERIFY_CLIENTS=$6
-
 
 while :
 do
-wanip=$(cat /app/ip.txt)
+touch ip.txt
+wanip=$(cat ip.txt)
 echo $wanip
 IP=`curl icanhazip.com`
  echo $IP
@@ -17,16 +11,8 @@ if [ "$wanip" = "$IP" ];then
 echo "ip ok"
 else
 echo "ip err"
-echo $IP >/app/ip.txt
-     pidlist=`ps -ef |grep derper |grep -v grep|awk '{print $2}'`
-     kill -9 $pidlist
-     /app/derper --hostname=$DERP_HOST \
-    --certmode=manual \
-    --certdir=$DERP_CERTS \
-    --stun=$DERP_STUN  \
-    --a=$DERP_ADDR \
-    --http-port=$DERP_HTTP_PORT \
-    --verify-clients=$DERP_VERIFY_CLIENTS
+echo $IP > ip.txt
+     docker restart derper
 fi
  
 done

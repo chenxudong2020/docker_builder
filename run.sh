05353079602c155602d20225323264a815f7c5b7
chenxudong2020 <872603935@qq.com>
2023-05-14 05:57:21 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
 DERP_HOST=$1
 DERP_CERTS=$2
 DERP_STUN=$3
 DERP_ADDR=$4
 DERP_HTTP_PORT=$5
 DERP_VERIFY_CLIENTS=$6
-
-first
-ipMoniter
 
 
 function first(){
@@ -54,3 +50,7 @@ else
   fi
 done
 }
+
+
+first
+ipMoniter

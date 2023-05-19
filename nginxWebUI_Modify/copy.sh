0c609c7304d88d163c9fb1f485e45a2897eeb2b6
chenxudong2020 <872603935@qq.com>
2023-05-19 08:59:48 +0000

    add

--- nginxWebUI_Modify/copy.sh
+++ nginxWebUI_Modify/copy.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+cp -rf ./CertController.java ../nginxWebUI/src/main/java/com/cym/controller/adminPage
+cp -rf ./ConfController.java ../nginxWebUI/src/main/java/com/cym/controller/adminPage
+cp -rf ./pom.xml ../nginxWebUI/

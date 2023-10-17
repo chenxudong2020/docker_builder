34bc59422a5b3513055f12df90a493cd2b85c067
chenxudong2020 <872603935@qq.com>
2023-10-17 06:11:09 +0000

    fix

--- nginxWebUI_Modify/copy.sh
+++ nginxWebUI_Modify/copy.sh
@@ -3,6 +3,6 @@
 \cp -rf ./ConfController.java ../nginxWebUI/src/main/java/com/cym/controller/adminPage
 \cp -rf ./Dockerfile ../nginxWebUI/
 \cp -rf ./pom.xml ../nginxWebUI/
-\chmod +x reload.sh
+chmod +x reload.sh
 \cp -rf ./reload.sh ../nginxWebUI/
 echo '---------------替换文件成功------------------'

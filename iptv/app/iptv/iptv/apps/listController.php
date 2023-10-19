aa2cb7165e803d4823d1c7dedc6df5cf86fb9fe3
chenxudong2020 <872603935@qq.com>
2023-10-19 12:03:53 +0800

    Update listController.php

--- iptv/app/iptv/iptv/apps/listController.php
+++ iptv/app/iptv/iptv/apps/listController.php
@@ -14,9 +14,9 @@ if (!isset($_SESSION['psw']) || $_SESSION['psw'] != $psw) {
 } 
 
 ?>
-
 <?php
 header("Content-type:text/json;charset=utf-8");
+
 function echoSource($category) {
     global $db;
     $db->mQuery("SET NAMES UTF8");

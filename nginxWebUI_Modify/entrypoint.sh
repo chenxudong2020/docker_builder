73fcf21873a633ff25a81c4780a273040f6b39b9
chenxudong2020 <872603935@qq.com>
2023-10-28 13:16:35 +0800

    add

---
+++ nginxWebUI_Modify/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /home
+exec java -jar -Dfile.encoding=UTF-8 -Xmx64m nginxWebUI.jar ${BOOT_OPTIONS} > /dev/null & \
+/usr/local/bin/tailscale.sh
--- nginxWebUI_Modify/reload.sh
+++ nginxWebUI_Modify/reload.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #续签证书成功需要执行的操作请现在下面
+tailscale down && tailscale up

3a4709bd2b7ad53177f08b292a29b7e8ebd36850
chenxudong2020 <872603935@qq.com>
2023-11-12 15:35:30 +0800

    ddddd

--- unas/tempature.sh
+++ unas/tempature.sh
@@ -4,29 +4,29 @@ while true
 do
     cpu_temp=$(sensors | grep -i "Core 0" | awk '{print $3}' | grep -oP "\d+\.\d+")
    if (( $(echo "$cpu_temp >= 75" | bc -l) )); then
-        echo "大于等于75"
+        $IS_DEBUG || echo "大于等于75"
 	    i2cset -y 0 0x54 0xf0 255
     elif (( $(echo "$cpu_temp >= 65" | bc -l) )) && (( $(echo "$cpu_temp < 75" | bc -l) )); then
-        echo "大于65小于等于75"
+        $IS_DEBUG || echo "大于65小于等于75"
 	   i2cset -y 0 0x54 0xf0 180
     elif (( $(echo "$cpu_temp >= 55" | bc -l) )) && (( $(echo "$cpu_temp < 65" | bc -l) )); then
-        echo "大于等于55小于65"
+        $IS_DEBUG || echo "大于等于55小于65"
 		i2cset -y 0 0x54 0xf0 150
     else
-		 echo "55度以下"
+		 $IS_DEBUG || echo "55度以下"
 		 i2cset -y 0 0x54 0xf0 100
 	fi
 	sleep 60
 done
 
 
-function tempature_off(){
+tempature_off(){
    if [ -f "/app/tempature_off.sh" ]; then
-         echo "退出执行 tempature_off.sh."
+         $IS_DEBUG || echo "退出执行 tempature_off.sh."
          bash /app/tempature_off.sh
          exit
      else
-         echo "stop file not exist"
+         $IS_DEBUG || echo "tempature_off.sh不存在"
          exit
      fi
 }

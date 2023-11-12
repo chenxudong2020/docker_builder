9408a4516bb915f97f87fa78253c356bb8a5448f
chenxudong2020 <872603935@qq.com>
2023-11-12 12:02:23 +0800

    fff

--- unas/tempature.sh
+++ unas/tempature.sh
@@ -2,16 +2,19 @@
 
 while true
 do
-    # 获取CPU温度
-    cpu_temp=$(sensors | grep -i "Core 0" | awk '{print $3}' | grep -oP "\d+\.\d+")
-    if (( $(echo "$cpu_temp >= 60" | bc -l) )); then
-        i2cset -y 0 0x54 0xf0 255
-    elif (( $(echo "$cpu_temp >= 50" | bc -l) )) && (( $(echo "$cpu_temp < 60" | bc -l) )); then
-        i2cset -y 0 0x54 0xf0 200
-    elif (( $(echo "$cpu_temp >= 40" | bc -l) )) && (( $(echo "$cpu_temp < 50" | bc -l) )); then
-        i2cset -y 0 0x54 0xf0 100
-    fi
-     echo "默认温度"
-     i2cset -y 0 0x54 0xf0 80
-    sleep 60
+    cpu_temp=$(/opt/sbin/sensors | grep -i "Core 0" | awk '{print $3}' | grep -oP "\d+\.\d+")
+   if (( $(echo "$cpu_temp >= 75" | /opt/bin/bc -l) )); then
+        echo "大于等于75"
+	    i2cset -y 0 0x54 0xf0 255
+    elif (( $(echo "$cpu_temp >= 65" | /opt/bin/bc -l) )) && (( $(echo "$cpu_temp < 75" | /opt/bin/bc -l) )); then
+        echo "大于65小于等于75"
+	   i2cset -y 0 0x54 0xf0 180
+    elif (( $(echo "$cpu_temp >= 55" | /opt/bin/bc -l) )) && (( $(echo "$cpu_temp < 65" | /opt/bin/bc -l) )); then
+        echo "大于等于55小于65"
+		i2cset -y 0 0x54 0xf0 150
+    else
+		 echo "55度以下"
+		 i2cset -y 0 0x54 0xf0 100
+	fi
+	sleep 60
 done

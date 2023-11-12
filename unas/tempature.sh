97abe0d1ac3485223a596ccdceede8c257f7df5d
chenxudong2020 <872603935@qq.com>
2023-11-12 16:09:34 +0800

    fix

--- unas/tempature.sh
+++ unas/tempature.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
+IS_LOG=$IS_DEBUG!
+
 while true
 do
     cpu_temp=$(sensors | grep -i "Core 0" | awk '{print $3}' | grep -oP "\d+\.\d+")
    if (( $(echo "$cpu_temp >= 75" | bc -l) )); then
-        $IS_DEBUG || echo "大于等于75"
+        eslog "大于等于75"
 	    i2cset -y 0 0x54 0xf0 255
     elif (( $(echo "$cpu_temp >= 65" | bc -l) )) && (( $(echo "$cpu_temp < 75" | bc -l) )); then
-        $IS_DEBUG || echo "大于65小于等于75"
-	   i2cset -y 0 0x54 0xf0 180
+        eslog "大于65小于等于75"
+	   i2cset -y 0 0x54 0xf0 200
     elif (( $(echo "$cpu_temp >= 55" | bc -l) )) && (( $(echo "$cpu_temp < 65" | bc -l) )); then
-        $IS_DEBUG || echo "大于等于55小于65"
+        eslog "大于等于55小于65"
 		i2cset -y 0 0x54 0xf0 150
     else
-		 $IS_DEBUG || echo "55度以下"
+		 eslog "55度以下"
 		 i2cset -y 0 0x54 0xf0 100
 	fi
 	sleep 60
 done
 
 
-tempature_off(){
-   if [ -f "/app/tempature_off.sh" ]; then
-         $IS_DEBUG || echo "退出执行 tempature_off.sh."
-         bash /app/tempature_off.sh
+function eslog(){
+    if [ "$IS_DEBUG" == false ]; then
+        echo "$*"
+    fi    
+}
+
+
+function tempature_off(){
+   if [ -f "tempature_off.sh" ]; then
+         eslog "退出执行 tempature_off.sh."
+         bash tempature_off.sh
          exit
      else
-         $IS_DEBUG || echo "tempature_off.sh不存在"
+         eslog "tempature_off.sh不存在"
          exit
      fi
 }

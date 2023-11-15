2687407b5d85908f84655713a2e91e6ccbb49c87
chenxudong2020 <872603935@qq.com>
2023-11-15 21:11:41 +0800

    fffff

--- Docker-CodeWeavers_CrossOver-VNC-Modify/BaiduNetdisk/entrypoint.sh
+++ Docker-CodeWeavers_CrossOver-VNC-Modify/BaiduNetdisk/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-
-
-
 if [ -f "${HOME}/baidunetdiskdownload/.vnc/passwd.txt" ]; then
     vnc_password=$(cat "${HOME}/baidunetdiskdownload/.vnc/passwd.txt")
 fi
@@ -23,4 +20,25 @@ if [ -z $vnc_password ]; then
     /usr/bin/vncserver -geometry 1920x1080 -fg -SecurityTypes None,TLSNone
 else
     /usr/bin/vncserver -geometry 1920x1080 -fg
+fi
+
+if [ -f "${HOME}/baidunetdiskdownload/.vnc/${DOMAIN}.crt" ]; then 
+    /bin/echo -e 'alias ll="ls -last"' >> ${HOME}/.bashrc
+    mkdir -p ${HOME}/.fluxbox
+    /bin/echo -e "session.screen0.toolbar.placement: TopCenter" >> ${HOME}/.fluxbox/init
+    /bin/echo -e "session.screen0.workspaces:     1 ">> ${HOME}/.fluxbox/init
+    /bin/echo -e "export DISPLAY=${DISPLAY}"  >> ${HOME}/.vnc/xstartup
+    /bin/echo -e "[ -r ${HOME}/.Xresources ] && xrdb ${HOME}/.Xresources\nfbsetroot -solid gray"  >> ${HOME}/.vnc/xstartup
+    /bin/echo -e "fluxbox &"  >> ${HOME}/.vnc/xstartup
+    /bin/echo -e 'export GTK_IM_MODULE=fcitx' >> ${HOME}/.vnc/xstartup
+    /bin/echo -e 'export QT_IM_MODULE=fcitx' >> ${HOME}/.vnc/xstartup
+    /bin/echo -e 'export XMODIFIERS="@im=fcitx"' >> ${HOME}/.vnc/xstartup
+    /bin/echo -e "sleep 3"  >> ${HOME}/.vnc/xstartup
+    /bin/echo -e "fcitx"  >> ${HOME}/.vnc/xstartup
+    /bin/echo -e  "/opt/noVNC-1.4.0/utils/novnc_proxy --listen 6080 --cert ${HOME}/.vnc/${DOMAIN}.crt --key ${HOME}/.vnc/${DOMAIN}.key --vnc ${DOMAIN}:5901 &"
+    /bin/echo -e "sudo mkdir -p /home/baidu/baidunetdiskdownload/"  >> ${HOME}/.vnc/xstartup
+    /bin/echo -e "sudo chmod -R a=rwx /home/baidu/baidunetdiskdownload/"  >> ${HOME}/.vnc/xstartup
+    /bin/echo -e "while true; do" >> ${HOME}/.vnc/xstartup
+    /bin/echo -e "    /opt/baidunetdisk/baidunetdisk --no-sandbox >> /var/log/baidunetdisk.log 2>&1" >> ${HOME}/.vnc/xstartup
+    /bin/echo -e "done" >> ${HOME}/.vnc/xstartup
 fi

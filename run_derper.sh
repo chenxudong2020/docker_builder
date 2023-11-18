66aa63fb3eb57092f62010eae45699482877ef76
chenxudong2020 <872603935@qq.com>
2023-11-18 08:52:27 +0800

    fff

--- run_derper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-     /app/derper --hostname=$DERP_HOST \
-    --certmode=manual \
-    --certdir=$DERP_CERTS \
-    --stun=$DERP_STUN  \
-    --a=$DERP_ADDR \
-    --http-port=$DERP_HTTP_PORT \
-    --verify-clients=$DERP_VERIFY_CLIENTS
